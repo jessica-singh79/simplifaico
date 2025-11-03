04eecb9c6a1eb224ac3e8d0e8b6b4741e0d9f832
Jessica Singh <145717016+jessica-singh79@users.noreply.github.com>
2025-11-02 20:35:48 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-// https://vitejs.dev/config/
+import path from "path";
 export default defineConfig({
-  plugins: [react()],
+  // ...
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
