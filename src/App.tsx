d334d451c94b703e3f8d9bf6a4d0c0f8f91d2312
Jessica Singh <145717016+jessica-singh79@users.noreply.github.com>
2025-11-06 20:45:25 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
+import RealExamples from './components/RealExamples';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBarDemo />
       <Hero />
       <About />
+      <RealExamples />
       <HowItWorks />
       <Footer />
     </div>
